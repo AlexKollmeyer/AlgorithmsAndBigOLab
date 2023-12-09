1cdf7994449ed54fb946b6f5ed5792dde5cfddff
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-12-08 18:26:27 -0600

    Created and tested EvenOrOdd function

--- Algortithms&BigOLab/Program.cs
+++ Algortithms&BigOLab/Program.cs
@@ -1,10 +1,22 @@
 ﻿namespace Algortithms_BigOLab
 {
     internal class Program
-    {
+
+    {static bool EvenOrOdd (int value)
+            {
+            if (value % 2 == 0)
+            {
+                return (true);
+            }
+            else return (false);
+            }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(EvenOrOdd(1));
+            Console.WriteLine(EvenOrOdd(2));
+            Console.WriteLine(EvenOrOdd(35));
+            Console.WriteLine(EvenOrOdd(16));
         }
     }
 }
