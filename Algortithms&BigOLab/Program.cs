b347272dc1f5eba70f7acd7c6bcc8a7f5d74d3b1
Alex Kollmeyer <akollmeyer123@gmail.com>
2023-12-09 22:23:37 -0600

    Completed and tested the SortedNumberList Function

--- Algortithms&BigOLab/Program.cs
+++ Algortithms&BigOLab/Program.cs
@@ -1,26 +1,26 @@
 ﻿namespace Algortithms_BigOLab
 {
     internal class Program
-        
+
     // O(1) Constant
-    {static bool EvenOrOdd (int value)
-            {
+    { static bool EvenOrOdd(int value)
+        {
             if (value % 2 == 0)
             {
                 return true;
             }
             else return false;
-            }
+        }
         // O(n) Linear
-        static bool LessThan100 (List<int> ListOfNumbers)
+        static bool LessThan100(List<int> ListOfNumbers)
         {
             foreach (var item in ListOfNumbers)
             {
-                if(item >= 100)
+                if (item >= 100)
                 {
                     return false;
                 }
-                
+
             }
             return true;
         }
@@ -41,6 +41,28 @@
             }
             return false;
         }
+        // O(n^2) 
+        static List<int> SortedNumberList(List<int> OrginalList){
+            List<int> sortedList = new List<int> { };
+            foreach (var item in OrginalList)
+            {
+                bool sorted = false;
+                for (int i = 0; i < sortedList.Count; i++)
+                {
+                    if (item <= sortedList[i])
+                    {
+                        sortedList.Insert(i, item);
+                        sorted = true;
+                        break;
+                    }
+                }
+                if (sorted == false)
+                {
+                    sortedList.Add(item);
+                }
+            }
+            return sortedList;
+        }
         static void Main(string[] args)
         {
 
@@ -56,6 +78,26 @@
             List<string> NameList2 = new List<string> { "Ashley", "Arron", "Alex", "Dave", "Ashley" };
             Console.WriteLine(RepeatedNames(NameList1));
             Console.WriteLine(RepeatedNames(NameList2));
+            List<int> NumList1 = new List<int> { 5, 22, 1, 33, 16, 88, 2 };
+            List<int> NumList2 = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> NumList3 = new List<int> { 100, 0, 15, 33, 16, 88, 2 };
+            NumList1 = SortedNumberList(NumList1);
+            NumList2 = SortedNumberList(NumList2);
+            NumList3 = SortedNumberList(NumList3);
+            foreach (var item in NumList1)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in NumList2)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in NumList3)
+            {
+                Console.WriteLine(item);
+            }
+
+
         }
     }
 }
